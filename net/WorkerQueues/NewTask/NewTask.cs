4cd552040315e2df6c251fb564fd3887b1a99b0e
Carles Sanchez <carles.sanchez@col.vueling.com>
2023-02-12 12:29:34 +0100

    Adds persistance and manual ACK to queues on weroker queues example

--- net/WorkerQueues/NewTask/NewTask.cs
+++ net/WorkerQueues/NewTask/NewTask.cs
@@ -1,33 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using System.Text;
+﻿using System.Text;
 using RabbitMQ.Client;
-
-Console.WriteLine("Hello, World!");
-
 
 var factory = new ConnectionFactory { HostName = "localhost" };
 using var connection = factory.CreateConnection();
 using var channel = connection.CreateModel();
 
-channel.QueueDeclare(queue: "hello",
-    durable: false,
+channel.QueueDeclare(queue: "task_queue",
+    durable: true,
     exclusive: false,
     autoDelete: false,
     arguments: null);
 
 var message = GetMessage(args);
-
-
 var body = Encoding.UTF8.GetBytes(message);
 
+var properties = channel.CreateBasicProperties();
+properties.Persistent = true;
+
 channel.BasicPublish(exchange: string.Empty,
-    routingKey: "hello",
-    basicProperties: null,
+    routingKey: "task_queue",
+    basicProperties: properties,
     body: body);
 Console.WriteLine($" [x] Sent {message}");
 
 Console.WriteLine(" Press [enter] to exit.");
 Console.ReadLine();
 
-static string GetMessage(string[] args) =>(args.Length > 0 ? string.Join(" ", args) : "Hello World!");
+static string GetMessage(string[] args)
+{
+    return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+}
